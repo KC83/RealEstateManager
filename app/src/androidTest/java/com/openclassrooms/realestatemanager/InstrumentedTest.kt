3c9382a26216fe9233438b203c3895075b131a39
KC83 <kellyc83780@gmail.com>
2021-06-11 11:14:25 +0200

    Instrumented Test for Internet connection

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/InstrumentedTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/InstrumentedTest.kt
@@ -1,11 +1,14 @@
 package com.openclassrooms.realestatemanager
 
 import android.content.Context
+import android.view.Gravity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
@@ -13,8 +16,6 @@ import com.nhaarman.mockito_kotlin.given
 import com.openclassrooms.realestatemanager.di.module
 import com.openclassrooms.realestatemanager.ui.list.EstateListActivity
 import com.openclassrooms.realestatemanager.utils.tools.InternetManager
-import org.hamcrest.Matchers.not
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,11 +24,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.core.logger.Level
-import org.koin.test.AutoCloseKoinTest
 import org.koin.test.KoinTest
-import org.koin.test.KoinTestRule
 import org.koin.test.mock.MockProviderRule
 import org.koin.test.mock.declareMock
 import org.mockito.Mockito
@@ -57,6 +54,7 @@ class InstrumentedTest: KoinTest {
         }
     }
 
+    // TEST FOR OPEN THE MAPS IF WE HAVE A INTERNET CONNECTION
     @Test
     fun openMapsActivity_returnTrue() {
         declareMock<InternetManager> {
@@ -66,6 +64,7 @@ class InstrumentedTest: KoinTest {
         onView(withId(R.id.map_fragment)).check(matches(isDisplayed()))
     }
 
+    // TEST FOR NOT OPEN THE MAPS IF WE DON'T HAVE A INTERNET CONNECTION
     @Test
     fun openMapsActivity_returnFalse() {
         declareMock<InternetManager> {
@@ -74,4 +73,31 @@ class InstrumentedTest: KoinTest {
         onView(withId(R.id.btn_maps)).perform(click())
         onView(withId(R.id.estate_list)).check(matches(isDisplayed()))
     }
+
+    /*
+    @Test
+    fun checkToastMessage_returnTrue() {
+        declareMock<InternetManager> {
+            given(isConnected()).willReturn(true)
+        }
+
+        onView(withId(R.id.bottom_app_bar))
+            .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+            .perform(DrawerActions.open()) // Open Drawer
+
+        onView(withId(R.id.navigation_view))
+            .perform(NavigationViewActions.navigateTo(R.id.btn_internet));
+
+
+        /*
+        openActionBarOverflowOrOptionsMenu(context);
+        onView(withId(R.id.btn_internet)).perform(click())
+        */
+
+        //onView(withId(R.id.bottom_app_bar)).perform(NavigationViewActions.navigateTo(R.id.btn_internet));
+        //onView(withId(R.id.navigation_view)).perform(NavigationViewActions.navigateTo(R.id.btn_internet));
+        //onView(withText(R.string.internet)).perform(click())
+        //onView(withText("Vérification de la connexion Internet")).perform(click())
+
+    }*/
 }
